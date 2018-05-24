7340228cbbdfbdb0098d81517a2839737d3ebb30
Roman Sztergbaum <roman.sztergbaum@nokia.com>
2018-05-24 13:24:24 +0200

    add coverage.cmake

---
+++ cmake/coverage.cmake
@@ -0,0 +1,6 @@
+if (PROJECT_COVERAGE)
+    if (GCC)
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} --coverage")
+        message(STATUS "Coverage enabled new cxxflags are ${CMAKE_CXX_FLAGS_DEBUG}")
+    endif()
+endif()
