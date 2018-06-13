951df9fe43dd770ffc5fcd481cd908cc6641cba9
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-13 12:47:34 +0200

    minor fix, and add a minimal example

---
+++ examples/common/CMakeSources.cmake
@@ -0,0 +1,14 @@
+set(MODULE_PATH
+        ${CMAKE_CURRENT_SOURCE_DIR})
+
+set(MODULE_PUBLIC_HEADERS
+        "${MODULE_PATH}/systems/example_post_system.hpp"
+        "${MODULE_PATH}/systems/all_systems.hpp"
+        )
+
+set(MODULE_PRIVATE_HEADERS
+        "")
+
+set(MODULE_SOURCES
+        ${MODULE_PUBLIC_HEADERS}
+        ${MODULE_PRIVATE_HEADERS})
