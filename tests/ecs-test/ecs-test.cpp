3247a15747cb4ff6df3b01dba5d5966629c5c37b
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-15 16:01:51 +0200

    coverage

--- tests/ecs-test/ecs-test.cpp
+++ tests/ecs-test/ecs-test.cpp
@@ -210,10 +210,11 @@ TEST_F(fixture_system, size_per_system_type)
 
 TEST_F(fixture_system, load_plugins_from_non_existent_directory)
 {
-    shiva::fs::copy("systems", "save", shiva::fs::copy_options::recursive);
+    shiva::fs::copy("systems", "save", shiva::fs::copy_options::recursive | shiva::fs::copy_options::overwrite_existing);
     shiva::fs::remove_all("systems");
     ASSERT_FALSE(system_manager_.load_plugins());
-    shiva::fs::copy("save", "systems", shiva::fs::copy_options::recursive);
+    shiva::fs::copy("save", "systems", shiva::fs::copy_options::recursive | shiva::fs::copy_options::overwrite_existing);
+    shiva::fs::remove_all("save");
 }
 
 TEST_F(fixture_system, fake_plugin)
