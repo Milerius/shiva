99d2daa4035fa2cb90f3e08edf51bb3d537e1ef7
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-22 18:48:11 +0200

    update lua fixture name

--- tests/scripting-lua-test/script-lua-test.cpp
+++ tests/scripting-lua-test/script-lua-test.cpp
@@ -8,7 +8,7 @@
 #include <shiva/ecs/components/all.hpp>
 #include <systems/all_systems.hpp>
 
-class fixture_world : public ::shiva::world, public ::testing::Test
+class fixture_scripting : public ::shiva::world, public ::testing::Test
 {
 public:
     using systems_list = shiva::meta::type_list<shiva::scripting::lua_system,
@@ -24,7 +24,7 @@ protected:
         system_ptr = std::addressof(system_manager_.create_system<shiva::scripting::lua_system>("scripts_lua_tests"));
         system_ptr->register_entity_registry();
         system_ptr->register_components(shiva::ecs::common_components{});
-        system_ptr->register_types_list(fixture_world::systems_list{});
+        system_ptr->register_types_list(fixture_scripting::systems_list{});
         system_ptr->register_world();
         system_ptr->load_script("basic_tests.lua");
         system_ptr->update();
@@ -39,21 +39,21 @@ protected:
     shiva::scripting::lua_system *system_ptr{nullptr};
 };
 
-TEST_F(fixture_world, create_entity)
+TEST_F(fixture_scripting, create_entity)
 {
     sol::state &state = system_ptr->get_state();
     shiva::entt::entity_registry::entity_type id = state["test_create_entity"]();
     ASSERT_EQ(id, 0);
 }
 
-TEST_F(fixture_world, destroy_entity)
+TEST_F(fixture_scripting, destroy_entity)
 {
     sol::state &state = system_ptr->get_state();
     bool res = state["test_destroy_entity"]();
     ASSERT_TRUE(res);
 }
 
-TEST_F(fixture_world, components)
+TEST_F(fixture_scripting, components)
 {
     sol::state &state = system_ptr->get_state();
     bool res = state["test_component"]();
