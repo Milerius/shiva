cb1fbbb3f1d095dd4ed8b3b51834cbee7cbe284b
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-23 08:10:46 +0200

    update lua

--- modules/entt/shiva/entt/entt.hpp
+++ modules/entt/shiva/entt/entt.hpp
@@ -15,58 +15,8 @@ namespace shiva::entt
 
     //using entity_registry = ::entt::DefaultRegistry;
 
-    class entity_registry
+    class entity_registry : public ::entt::DefaultRegistry
     {
-    public:
-        using entity_type = ::entt::DefaultRegistry::entity_type;
-
-        template <typename Func>
-        void each(Func func) const
-        {
-            internal_registry_.each(func);
-        }
-
-        void destroy(const entity_type entity)
-        {
-            internal_registry_.destroy(entity);
-        }
-
-        entity_type create() noexcept
-        {
-            return internal_registry_.create();
-        }
-
-        template <typename Component>
-        const Component &get(const entity_type entity) const noexcept
-        {
-            return internal_registry_.get<Component>(entity);
-        }
-
-        template <typename Component>
-        Component &get(const entity_type entity) noexcept
-        {
-            return internal_registry_.get<Component>(entity);
-        }
-
-        template <typename Component, typename... Args>
-        Component &assign(const entity_type entity, Args &&... args)
-        {
-            return internal_registry_.assign<Component>(entity, std::forward<Args>(args)...);
-        }
-
-        template<typename... Component>
-        bool has(const entity_type entity) const noexcept
-        {
-            return (internal_registry_.has<Component>(entity) && ...);
-        }
-
-        template<typename Component>
-        void remove(const entity_type entity)
-        {
-            internal_registry_.remove<Component>(entity);
-        }
-
-
     public:
         //! Reflection
         reflect_class(entity_registry);
@@ -81,8 +31,5 @@ namespace shiva::entt
         {
             return meta::makeMap();
         }
-
-    private:
-        ::entt::DefaultRegistry internal_registry_;
     };
 }
