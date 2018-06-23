54d7a5cdfd27ddd1e0029706cf614a305c88ee95
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-23 13:35:54 +0200

    update lua

---
+++ tests/scripting-lua-test/scripts_lua_tests/systems/pre_example_system.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: romansztergbaum
+-- Date: 23/06/2018
+-- Time: 08:20
+-- To change this template use File | Settings | File Templates.
+--
+
+function internal_update()
+    print("pre_example_system")
+end
+
+pre_example_system_table = {
+    update = internal_update,
+    current_system_type = system_type.pre_update
+}
+
