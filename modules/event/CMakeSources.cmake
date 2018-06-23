4c1a7b19b1497b602de1305a8b488c93a1e8e902
roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-06-23 10:16:22 +0200

    add scripted_system

--- modules/event/CMakeSources.cmake
+++ modules/event/CMakeSources.cmake
@@ -2,6 +2,7 @@ set(MODULE_PATH
         ${CMAKE_CURRENT_SOURCE_DIR}/shiva/event)
 
 set(MODULE_PUBLIC_HEADERS
+        "${MODULE_PATH}/add_base_system.hpp"
         "${MODULE_PATH}/fatal_error_occured.hpp"
         "${MODULE_PATH}/quit_game.hpp"
         "${MODULE_PATH}/start_game.hpp"
---
+++ modules/lua/shiva/lua/scripted_system.hpp
@@ -0,0 +1,62 @@
+//
+// Created by roman Sztergbaum on 23/06/2018.
+//
+
+#pragma once
+
+#include <type_traits>
+#include <sol/state.hpp>
+#include <shiva/ecs/system.hpp>
+#include <shiva/filesystem/filesystem.hpp>
+
+namespace shiva::ecs
+{
+    template <typename SystemType>
+    class scripted_system : public system<scripted_system<SystemType>, SystemType>
+    {
+    public:
+        using TSystem = system <scripted_system<SystemType>, SystemType>;
+
+        scripted_system(shiva::entt::dispatcher &dispatcher,
+                        shiva::entt::entity_registry &entity_registry,
+                        const float &fixed_delta_time,
+                        sol::state &state,
+                        std::string table_name,
+                        std::string class_name) noexcept :
+            TSystem::system(dispatcher, entity_registry, fixed_delta_time),
+            state_(state),
+            table_name_(std::move(table_name))
+        {
+            class_name_ = std::move(class_name);
+        }
+
+        void update() noexcept override
+        {
+            state_[table_name_]["update"]();
+        }
+
+        static const std::string &class_name() noexcept
+        {
+            return class_name_;
+        }
+
+        static constexpr auto reflected_functions() noexcept
+        {
+            return meta::makeMap();
+        }
+
+        static constexpr auto reflected_members() noexcept
+        {
+            return meta::makeMap();
+        }
+
+    private:
+        sol::state &state_;
+        std::string table_name_;
+        static inline std::string class_name_{""};
+    };
+
+    using post_scripted_system = scripted_system<shiva::ecs::system_post_update>;
+    using pre_scripted_system = scripted_system<shiva::ecs::system_pre_update>;
+    using logic_scripted_system = scripted_system<shiva::ecs::system_logic_update>;
+}
