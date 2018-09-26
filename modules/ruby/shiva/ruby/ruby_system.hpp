5d2022ea1efda0fed17c194c6451d319fade226e
Syl Eze <syleze.contact@gmail.com>
2018-09-26 17:42:41 +0200

    Implement ruby_system ctor and dtor
    Construct ruby vm inside ctor
    Destruct ruby vm inside dtor
    Add member which holds ruby scripts directory
    Add member which holds ruby systems scripts directory

--- modules/ruby/shiva/ruby/ruby_system.hpp
+++ modules/ruby/shiva/ruby/ruby_system.hpp
@@ -4,14 +4,52 @@
 
 #pragma once
 
+#include <ruby.h>
+
+#include <shiva/reflection/reflection.hpp>
+#include <shiva/filesystem/filesystem.hpp>
 #include <shiva/ecs/system.hpp>
 
 namespace shiva::ruby
 {
     class ruby_system : public ecs::logic_update_system<ruby_system>
     {
-        private:
-            template <typename Component>
-            void register_component_() noexcept;
+    public:
+        reflect_class(ruby_system)
+
+    public:
+        ruby_system(entt::dispatcher &dispatcher,
+                    entt::entity_registry &entity_registry,
+                    const float &fixed_delta_time,
+                    shiva::fs::path scripts_directory = shiva::fs::current_path() / "assets/scripts/ruby",
+                    shiva::fs::path systems_scripts_directory = shiva::fs::current_path() /
+                                                                "assets/scripts/systems/ruby") noexcept :
+          system(dispatcher, entity_registry, fixed_delta_time),
+          scripts_directory_(scripts_directory),
+          systems_scripts_directory_(systems_scripts_directory)
+        {
+          // Construct ruby VM
+          ruby_init();
+
+          log_->debug("ruby vm constructed.");
+        }
+
+        ~ruby_system() override
+        {
+          // Destruct ruby VM
+          ruby_cleanup(0);
+
+          log_->debug("ruby vm destroyed.");
+        }
+
+    public:
+        void update() noexcept override
+        {
+
+        }
+
+    private:
+        shiva::fs::path scripts_directory_;
+        shiva::fs::path systems_scripts_directory_;
     };
 }
